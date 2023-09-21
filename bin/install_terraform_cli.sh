74afb6da85863bd61ca95a3b839732cfd55f0a6b
Chukwuma Ibegbulem <ibechuksvictor@gmail.com>
2023-09-21 14:33:03 +0100

    Provided example for env var and documentation on how to set them properly (#6)

--- bin/install_terraform_cli.sh
+++ bin/install_terraform_cli.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+cd /workspace
+
 sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
 
 wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
@@ -14,3 +16,4 @@ sudo apt update
 
 sudo apt-get install terraform -y
 
+cd $PROJECT_ROOT
